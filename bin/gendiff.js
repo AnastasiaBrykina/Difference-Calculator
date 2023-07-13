29981b79c72892b0df7b0dbdddf741e604db95a6
AnastasiaBrykina <anastasiaaa.6934.@gmail.com>
2023-07-13 17:58:18 +0200

    update package.json package-lock.json gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import findDiff from '../src/index.js';
+
+const genDiff = (path1, path2) => findDiff(path1, path2);
 
 program
   .name('gendiff')
@@ -8,6 +11,11 @@ program
   .version('1.0.0')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .option('-f, --format <type>', 'output format');
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    genDiff(filepath1, filepath2);
+  });
 
 program.parse();
+
+export default genDiff;
