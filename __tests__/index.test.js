73577762066c2d3855bc36dd2bcc5163742f8769
AnastasiaBrykina <anastasiaaa.6934.@gmail.com>
2023-08-09 15:30:12 +0200

    update code

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,18 +1,26 @@
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
-import genDiff from '../src/index.js';
+import genDiff, { fileReader } from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 
-test('existing path', () => {
-  const path1 = getFixturePath('file1.json');
-  const path2 = getFixturePath('file2.json');
-  expect(genDiff(path1, path2)).toEqual(
-    '{\n - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true\n}',
-  );
+const expectedStylishResult = fileReader(getFixturePath('expectedStylishResult.txt'));
+const expectedPlainResult = fileReader(getFixturePath('expectedPlainResult.txt'));
+
+test('existing path-json', () => {
+  const path1 = getFixturePath('recFile1.json');
+  const path2 = getFixturePath('recFile2.json');
+  expect(genDiff(path1, path2)).toEqual(expectedStylishResult);
+  expect(genDiff(path1, path2, 'plain')).toEqual(expectedPlainResult);
+});
+
+test('existing path-yml', () => {
+  const path1 = getFixturePath('recFile1.yml');
+  const path2 = getFixturePath('recFile2.yml');
+  expect(genDiff(path1, path2)).toEqual(expectedStylishResult);
+  expect(genDiff(path1, path2, 'plain')).toEqual(expectedPlainResult);
 });
 
 test('non-existing path', () => {
@@ -21,4 +29,12 @@ test('non-existing path', () => {
   expect(() => {
     genDiff(path1, path2);
   }).toThrow('path non-exist');
+});
+
+test('unknown file type', () => {
+  const path1 = getFixturePath('unknownFile1.xml');
+  const path2 = getFixturePath('unknownFile2.xml');
+  expect(() => {
+    genDiff(path1, path2);
+  }).toThrow('Unknown type');
 });
--- src/index.js
+++ src/index.js
@@ -1,61 +1,31 @@
 import fs from 'fs';
 import path from 'path';
-import _ from 'lodash';
+import formatSelectioner from './formatters/index.js';
+import parseContent from './parsers.js';
+import makeTreeDiff from './makeTreeDiff.js';
 
-const parseContent = (content, type) => {
-  let result;
-
-  switch (type) {
-    case '.json':
-      result = JSON.parse(content);
-      break;
-    default:
-      throw new Error('Unknown type');
-  }
-
-  return result;
-};
-
-const readFile = (pathToFile) => {
-  let content;
+const fileReader = (pathToFile) => {
   try {
-    content = fs.readFileSync(pathToFile, 'utf8');
+    const content = fs.readFileSync(pathToFile, 'utf8');
+    return content;
   } catch {
     throw new Error('path non-exist');
   }
-
-  return content;
 };
 
-const genDiff = (path1, path2) => {
-  const absolutePath1 = path.resolve(path1);
-  const absolutePath2 = path.resolve(path2);
+const makeData = (pathToFile) => {
+  const absolute = path.resolve(pathToFile);
+  const content = fileReader(absolute);
+  const data = parseContent(content, path.extname(absolute));
 
-  const contentFile1 = readFile(absolutePath1);
-  const contentFile2 = readFile(absolutePath2);
+  return data;
+};
 
-  const data1 = parseContent(contentFile1, path.extname(absolutePath1));
-  const data2 = parseContent(contentFile2, path.extname(absolutePath2));
+const genDiff = (path1, path2, formatName = 'stylish') => {
+  const treeDiff = makeTreeDiff(makeData(path1), makeData(path2));
 
-  const keys = _.union(Object.keys(data1), Object.keys(data2));
-  const sortKeys = _.sortBy(keys, (key) => key[0]);
-
-  const lines = sortKeys.map((key) => {
-    if (!Object.hasOwn(data1, key)) {
-      return ` + ${key}: ${data2[key]}`;
-    }
-    if (!Object.hasOwn(data2, key)) {
-      return ` - ${key}: ${data1[key]}`;
-    }
-    if (data1[key] !== data2[key]) {
-      return ` - ${key}: ${data1[key]}\n + ${key}: ${data2[key]}`;
-    }
-    return `   ${key}: ${data1[key]}`;
-  });
-
-  const result = `{\n${lines.join('\n')}\n}`;
-
-  return result;
+  return formatSelectioner(treeDiff, formatName);
 };
 
 export default genDiff;
+export { fileReader };
