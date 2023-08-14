5a559e57526ae586670247901d3a8580af878935
AnastasiaBrykina <anastasiaaa.6934.@gmail.com>
2023-08-14 19:33:40 +0200

    add src/constants.js

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+const STATUS = {
+  added: 'added',
+  deleted: 'deleted',
+  changed: 'changed',
+  unchanged: 'unchanged',
+};
+
+const STYLISH_SYMBOLS = {
+  added: '+',
+  deleted: '-',
+  unchanged: ' ',
+};
+
+export { STATUS, STYLISH_SYMBOLS };
