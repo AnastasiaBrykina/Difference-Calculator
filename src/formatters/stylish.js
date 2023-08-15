8a0f3ab07ed70813f799431efa0180fd55c6f601
AnastasiaBrykina <anastasiaaa.6934.@gmail.com>
2023-08-15 13:24:07 +0200

    refactoring

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,46 +1,44 @@
 import _ from 'lodash';
 import { STATUS, STYLISH_SYMBOLS } from '../constants.js';
 
-const getIndent = (count) => ' '.repeat(count);
+const getIndent = (count, depth, shift = 0) => ' '.repeat(count * depth - shift);
+const getBracketIndent = (count, depth) => ' '.repeat(count * depth - count);
 
-const stringify = (value, indent, stepCount) => {
+const stringify = (value, depth, stepCount) => {
   if (!_.isObject(value)) {
     return `${value}`;
   }
 
-  const indentCount = indent + stepCount;
-  const bracketIndent = getIndent(indentCount - stepCount);
-  const lines = Object.entries(value).map(([key, val]) => `${getIndent(indentCount)}${key}: ${stringify(val, indentCount, stepCount)}`);
+  const indentCount = getIndent(stepCount, depth);
+  const lines = Object.entries(value).map(([key, val]) => `${indentCount}${key}: ${stringify(val, depth + 1, stepCount)}`);
 
-  return ['{', ...lines, `${bracketIndent}}`].join('\n');
+  return ['{', ...lines, `${getBracketIndent(stepCount, depth)}}`].join('\n');
 };
 
 const stylishFormatter = (tree, count) => {
   const iter = (node, depth) => {
-    const leftShift = 2;
-    const indentCount = depth * count - leftShift;
-    const bracketIndent = getIndent(depth * count - count);
+    const indentCount = getIndent(count, depth, 2);
 
     const lines = node.map((item) => {
       const { key, status, value } = item;
 
       if (Object.hasOwn(item, 'children')) {
         const { children } = item;
-        return `${getIndent(indentCount)}  ${key}: ${iter(children, depth + 1)}`;
+        return `${indentCount}  ${key}: ${iter(children, depth + 1)}`;
       }
 
       if (status === STATUS.changed) {
         const { to, from } = item;
-        const oldValue = stringify(from, indentCount + leftShift, count);
-        const newValue = stringify(to, indentCount + leftShift, count);
+        const oldValue = stringify(from, depth + 1, count);
+        const newValue = stringify(to, depth + 1, count);
 
-        return `${getIndent(indentCount)}${STYLISH_SYMBOLS.deleted} ${key}: ${oldValue}\n${getIndent(indentCount)}${STYLISH_SYMBOLS.added} ${key}: ${newValue}`;
+        return `${indentCount}${STYLISH_SYMBOLS.deleted} ${key}: ${oldValue}\n${indentCount}${STYLISH_SYMBOLS.added} ${key}: ${newValue}`;
       }
 
-      return `${getIndent(indentCount)}${STYLISH_SYMBOLS[status]} ${key}: ${stringify(value, indentCount + leftShift, count)}`;
+      return `${indentCount}${STYLISH_SYMBOLS[status]} ${key}: ${stringify(value, depth + 1, count)}`;
     });
 
-    return ['{', ...lines, `${bracketIndent}}`].join('\n');
+    return ['{', ...lines, `${getBracketIndent(count, depth)}}`].join('\n');
   };
 
   return iter(tree, 1);
